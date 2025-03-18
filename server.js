6278d8cfe1decfef8708e32e07cb157e06622a1c
MarsGotBars <marcinm@live.nl>
2025-03-18 17:08:14 +0100

    moved project

--- server.js
+++ server.js
@@ -1,74 +1,115 @@
-// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
-// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+import express from "express";
+import { Liquid } from "liquidjs";
 
-// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
-import { Liquid } from 'liquidjs';
+let taskData = [];
+let exerciseData = [];
+const fetchThemedTask = async () => {
+  const taskList = await fetch(
+    "https://fdnd-agency.directus.app/items/dropandheal_task"
+  );
+  // Skip hiermee het benoemen van 'data'
+  const { data: taskListJson } = await taskList.json();
+  taskData = taskListJson;
+};
 
-// Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+const fetchThemedExercise = async () => {
+  const exerciseList = await fetch(
+    "https://fdnd-agency.directus.app/items/dropandheal_exercise"
+  );
+  // Skip hiermee het benoemen van 'data'
+  const { data: exerciseListJson } = await exerciseList.json();
+  // Refresh de data voor de check
+  exerciseData = exerciseListJson;
+};
 
-// Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+(async () => {
+  await fetchThemedExercise();
+  await fetchThemedTask();
+})();
+const app = express();
 
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-// Stel Liquid in als 'view engine'
 const engine = new Liquid();
-app.engine('liquid', engine.express());
+app.engine("liquid", engine.express());
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
+app.set("views", "./views");
 
+app.get("/", async function (request, response) {
+  response.render("index.liquid");
+});
 
-console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
+// Dynamische parameter om te gebruiken bij het vinden van de correcte taskData
+app.get("/:theme", async function (request, response) {
+  // Custom lijst met alle task titles en pathNames
+  const tasks = taskData.map((task) => ({
+    title: task.title,
+    pathName: task.theme,
+    id: task.id,
+  }));
 
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
-app.get(…, async function (request, response) {
-  
-  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
-  response.render(…)
-})
-*/
+  // Sla deze op voor leesbaarheid
+  const requestedTheme = request.params.theme;
 
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
-app.post(…, async function (request, response) {
+  // Zoek taskData dmv de gevraagde :theme
+  const foundData = taskData.find((data) => data.theme === requestedTheme);
 
-  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
-  console.log(request.body)
+  if (!foundData) {
+    return response.status(404).render("err.liquid");
+  }
 
-  // Via een fetch() naar Directus vullen we nieuwe gegevens in
+  // Destructureer om props makkelijk door te
+  const { pathName, theme, title, id, exercise: exerciseList } = foundData;
 
-  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
-  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
-  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
-  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
-  await fetch(…, {
-    method: …,
-    body: JSON.stringify(…),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
+  const exerciseInfo = exerciseList.map((exercise) => {
+    return exerciseData.find((e) => e.id === exercise);
+  });
+  const exercises = exerciseInfo.map((exercise) => {
+    return {
+      ...exercise,
+      // absoluut pad, altijd handig -Krijn
+      pathName: `/${pathName}/${exercise.id}`,
+    };
   });
 
-  // Redirect de gebruiker daarna naar een logische volgende stap
-  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
-  response.redirect(303, …)
-})
-*/
+  // respond met de gevraagde pagina & het behorende thema
+  response.render(`task.liquid`, {
+    theme,
+    title,
+    id,
+    tasks,
+    exercises,
+  });
+});
 
+app.get("/:theme/:pageId", async function (request, response) {
+  const { theme, pageId } = request.params;
+  const foundData = taskData.find((data) => data.theme === theme);
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
+  // - 1 omdat we door een array lopend die start op 0
+  const exercise = exerciseData.find(
+    (exercise) => exercise.id === foundData.exercise[pageId - 1]
+  );
+  if (!exercise) {
+    return response.status(404).render("err.liquid");
+  }
+  const { title, description, image } = exercise;
+  const { foundtheme, id } = foundData;
+  response.render(`exercise.liquid`, {
+    foundtheme,
+    title,
+    description,
+    id,
+    image,
+  });
+});
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
-})
+app.post("/", async function (request, response) {
+  response.redirect(303, "/");
+});
+
+app.set("port", process.env.PORT || 8000);
+
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
