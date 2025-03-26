c069b8ccf80d93c6bf61aa8c4f0f8eaf7ae070b8
Marcin Magdziak <marcinm@live.nl>
2025-03-26 12:02:38 +0100

    check for dialog
    #11
    - kijk of dialog beschikbaar is in de browser, zo niet dan krijg je de default form onderaan

--- public/scripts/dialogFunctionality.js
+++ public/scripts/dialogFunctionality.js
@@ -1,3 +1,10 @@
-const dialog = document.querySelector("dialog");
-dialog.showModal();
-console.log(dialog);
+if ("HTMLDialogElement" in window) {
+  const form = document.querySelector("form");
+  const createdDialog = document.createElement("dialog");
+  document.body.append(createdDialog);
+  const dialog = document.querySelector("dialog");
+  dialog.appendChild(form);
+  dialog.showModal();
+} else {
+  console.log("Dialog is not supported");
+}
