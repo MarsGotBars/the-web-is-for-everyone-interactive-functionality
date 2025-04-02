92512e5b1fb72d401e2a18ce18f9c1d57371ec4c
MarsGotBars <marcinm@live.nl>
2025-04-02 18:01:33 +0200

    restructured script & changed from direct fetch to server fetch
    - Na wat uitleg van Krijn in #11 & Teams snap ik nu dat ik via mijn client script gebruik maak van de serverjs om mijn data op te halen zonder de pagina te refreshen.
    - Voor nu bevat het alleen de classList implementatie, ideaal maak ik gebruik van view transition API (to do) zal hier een issue voor maken #17
    - Belangrijk is het dat ik eigenlijk alleen de form & de nieuwe card fetch, dit om de form te 'refreshen' als het ware en alleen de nieuwe card in te voegen.
      - Als extra enhancement kan ik hier ook een fade-in aan geven.

--- public/scripts/dialogFunctionality.js
+++ public/scripts/dialogFunctionality.js
@@ -1,152 +1,113 @@
-if ("HTMLDialogElement" in window) {
-  const dialogOpen = document.querySelector(".opener");
-  const dialogClose = document.querySelector(".close");
-  const form = document.querySelector("form.dialog");
-  const dropContainer = document.querySelector(".drop-container");
-  if (dropContainer) {
-    dropContainer.style.display = "block";
-  }
-  if (form) {
-    const formSubmit = form.querySelector("button[value='false']");
-    const formConcept = form.querySelector("button[value='true']");
-    const isExercise = form.dataset.exercise === "true";
-    console.log(isExercise);
+const form = document.querySelector("form.dialog");
+if (form) {
+  const isExercise = form.dataset.exercise === "true";
+  // Dialog moet global zijn zodat deze ook gebruikt kan worden in de submit
+  let dialog;
+  // Als dialog beschikbaar is, maak ik een dialog aan
+  if ("HTMLDialogElement" in window) {
+    // Als er een dialog gemaakt wordt, toon ik de open button hiervoor met de if
+    const dropContainer = document.querySelector(".drop-container");
+
+    if (dropContainer) {
+      dropContainer.style.display = "block";
+    }
+
     const createdDialog = document.createElement("dialog");
     document.body.append(createdDialog);
-    const dialog = document.querySelector("dialog");
+    dialog = document.querySelector("dialog");
     dialog.appendChild(form);
 
+    // Dedicated buttons voor dialog openen en sluiten
+    const dialogOpen = document.querySelector(".opener");
+    const dialogClose = document.querySelector(".close");
     dialogOpen.addEventListener("click", (e) => {
-      // Open modal
       dialog.showModal();
-      // Na het openen wil ik er zeker van zijn dat ik niet door link
       e.preventDefault();
     });
 
-    // Mogelijk dat deze onnodig is en ik wel direct terug naar de task wil
     dialogClose.addEventListener("click", (e) => {
       dialog.close();
       e.preventDefault();
     });
+  }
+  // Als het geen exercise is en fetch en DOMParser beschikbaar zijn, gebruik ik fetch om de data te versturen.
+  if (!isExercise && "fetch" in window && "DOMParser" in window) {
+    document.addEventListener("submit", async (e) => {
+      form.classList.add("loading");
+      const submitForm = e.target;
+      const formData = new FormData(submitForm);
 
-    let clickedButton = null;
-    formSubmit.addEventListener(
-      "click",
-      () => (clickedButton = formSubmit.value)
-    );
-    formConcept.addEventListener(
-      "click",
-      () => (clickedButton = formConcept.value)
-    );
-    if (!isExercise) {
-      document.addEventListener("submit", async (e) => {
-        form.classList.add("loading");
-        const submitForm = e.target;
-        const formData = new FormData(submitForm);
-        const formDataObject = Object.fromEntries(formData);
+      if (e.submitter.value) {
+        formData.append(e.submitter.name, e.submitter.value);
+      }
 
-        const { message, person, anonymous, conceptId, exerciseId } = formDataObject;
-        console.log(formDataObject);
-        e.preventDefault();
-        if (conceptId) {
-          try {
-            const res = await fetch(
-              `https://fdnd-agency.directus.app/items/dropandheal_messages/${conceptId}`,
-              {
-                method: "PATCH",
-                headers: {
-                  "Content-Type": "application/json;charset=UTF-8",
-                },
-                body: JSON.stringify({
-                  text: message,
-                  from:
-                    anonymous && clickedButton !== "true" ? "anoniem" : person,
-                  date_created: new Date().toISOString(),
-                  concept: clickedButton,
-                }),
-              }
-            );
-            if (!res.ok) {
-              throw new Error("Network response was not ok");
-            }
-            dialog.close();
-          } catch (error) {
-            console.error("Error:", error);
-            form.classList.add("error");
-            form.classList.remove("loading");
-
-            setTimeout(() => {
-              dialog.classList.add("fade-out");
-              setTimeout(() => {
-                form.classList.remove("error");
-                dialog.classList.remove("fade-out");
-                dialog.close();
-              }, 1000);
-            }, 2000);
-            return;
-          }
-        } else {
-          console.log("else!");
-          try {
-            const res = await fetch(
-              "https://fdnd-agency.directus.app/items/dropandheal_messages",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json;charset=UTF-8",
-                },
-                body: JSON.stringify({
-                  exercise: exerciseId,
-                  text: message,
-                  from:
-                    anonymous && clickedButton !== "true" ? "anoniem" : person,
-                  concept: clickedButton,
-                }),
-              }
-            );
-
-            if (!res.ok) {
-              throw new Error("Network response was not ok");
-            }
-            const responseText = await res.text()
-            const parser = new DOMParser();
-            const responseDOM = parser.parseFromString(responseText, "text/html");
-            const main = responseDOM.querySelector("main");
-
-            // Close the dialog after successful submission
-            dialog.close();
-          } catch (error) {
-            console.error("Error:", error);
-            form.classList.remove("loading");
-            form.classList.add("error");
-            setTimeout(() => {
-              dialog.classList.add("fade-out");
-              setTimeout(() => {
-                form.classList.remove("error");
-                dialog.classList.remove("fade-out");
-                dialog.close();
-              }, 500);
-            }, 2000);
-            return;
-          }
-        }
-        console.log("success");
-        form.classList.remove("loading");
-        form.classList.add("success");
-        setTimeout(() => {
-        console.log("success");
-
-          dialog.classList.add("fade-out");
-          setTimeout(() => {
-        console.log("success");
-            form.classList.remove("success");
-            dialog.classList.remove("fade-out");
-            dialog.close();
-          }, 500);
-        }, 2000);
+      const formDataObject = new URLSearchParams(formData);
+      e.preventDefault();
+      try {
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: formDataObject,
       });
-    }
+      if (!response.ok) {
+        throw new Error("Response not ok!");
+      }
+      // Check of we geredirect worden naar de drops pagina of niet, zo weten we of we een nieuwe card moeten toevoegen of niet.
+      if (response.url.includes("/drops")) {
+        const responseText = await response.text();
+        const parser = new DOMParser();
+        const responseDOM = parser.parseFromString(responseText, "text/html");
+        const newCard = responseDOM.querySelector("article");
+        const main = document.querySelector("main");
+        // Als er een nieuwe card is, voeg deze toe aan de main
+        if (newCard) {
+          // Als er geen dialog is en de form is in de main, voeg de nieuwe card toe aan de main
+          if (!dialog && main.contains(form)) {
+            form.insertAdjacentElement("afterend", newCard);
+          } else {
+            main.insertAdjacentElement("afterbegin", newCard);
+          }
+          const newForm = responseDOM.querySelector("form.dialog");
+          setTimeout(() => {
+            form.classList.remove("loading");
+            form.classList.add("success");
+            setTimeout(() => {
+              form.classList.remove("success");
+              form.outerHTML = newForm.outerHTML;
+              if (dialog) {
+                dialog.close();
+              }
+            }, 1200);
+          }, 300);
+        }
+      } else {
+        console.log("hoi");
+
+        setTimeout(() => {
+          form.classList.remove("loading");
+          form.classList.add("success");
+          setTimeout(() => {
+            // Als er geredirect wordt, volg de redirect
+            window.location.href = response.url;
+          }, 1200);
+        }, 300);
+
+        return;
+        }
+      } catch (error) {
+        // console.log(error);
+        setTimeout(() => {
+          form.classList.remove("loading");
+          form.classList.add("error");
+          setTimeout(() => {
+            form.classList.remove("error");
+            if (dialog) {
+              dialog.close();
+            }
+          }, 1500);
+        }, 300);
+      }
+    });
   }
 } else {
-  console.log("Dialog is not supported");
+  console.log("Fetch or DOMParser is not supported");
 }
